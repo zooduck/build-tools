2b28b5e0a9bc6b4f432afc2e5c3375eccf497ce4
zooduck <zooduck@beniceoreatsomerice.com>
2023-06-08 21:50:35 +0100

    rename all .mjs file extensions to .js

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs/promises';
-import { buildTools } from './src/buildTools.module.mjs';
+import { buildTools } from './src/buildTools.module.js';
 
 await fs.rm('dist', { recursive: true, force: true });
 await fs.mkdir('dist');
-await fs.copyFile('src/buildTools.module.mjs', 'dist/index.module.mjs');
-await buildTools.removeCommentsFromFile('dist/index.module.mjs');
+await fs.copyFile('src/buildTools.module.js', 'dist/index.module.js');
+await buildTools.removeCommentsFromFile('dist/index.module.js');
